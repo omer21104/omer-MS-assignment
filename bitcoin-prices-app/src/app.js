f6b386e20940704d5882a44d147e6a2aef0c948d
omer zano <omer21104@gmail.com>
2024-01-21 15:45:42 +0200

    add webserver with readiness and health endpoints

--- bitcoin-prices-app/src/app.js
+++ bitcoin-prices-app/src/app.js
@@ -1,5 +1,24 @@
+const express = require("express");
 const { iterationHandler } = require("./helpers/main-helper");
 
+const app = express();
+const port = 3000;
 const minute = 60 * 1000;
+const intervalOverride = process.env.INTERVAL; // for testing with shorter interval
+const interval = intervalOverride || minute;
 
-setInterval(() => iterationHandler(), minute);
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.get("/ready", (req, res) => {
+  if (taskId) {
+    res.status(200).send("OK");
+  } else {
+    res.status(503).send("Service Unavailable");
+  }
+});
+
+const taskId = setInterval(() => iterationHandler(), interval);
+
+app.listen(port, () => console.log("listening on port: ", port));
